18dbc77dc68c6f9f49369ca42e8657b474df876b
Багаев Артур <aibagayev@mts.ru>
2022-11-20 13:17:04 +0300

    init commit

---
+++ src/main/java/com/pitaza170/currencytradingservice/CurrencytradingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pitaza170.currencytradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencytradingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencytradingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitaza170/currencytradingservice/CurrencytradingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitaza170.currencytradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencytradingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
